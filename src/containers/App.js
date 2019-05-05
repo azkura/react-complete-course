3863665eb75afd1d187a082bd655113cc85d55da
az <azizkoura@hotmail.com>
2019-05-05 14:18:42 +0200

    another form of Hocs (using withclass function)

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,8 @@ import React,{ Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit'
+import withClass from '../hoc/withClass'
+import Aux from '../hoc/Aux'
 
 class App extends Component{
   constructor(props){
@@ -69,16 +71,18 @@ class App extends Component{
     }
 
     return (
-        <div className={classes.App}>
+      <Aux>
+        
           <Cockpit
             title={this.props.appTitle}
             showPersons={this.state.showPersons}
             persons={this.state.persons}
             clicked={this.togglePresonHandler} />
           {persons}
-        </div>
+      
+       </Aux>
     );
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
